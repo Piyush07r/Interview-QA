afff36df675ba65f6d9eaaf028690cfd00aac14b
Piyush Kumar Singh <85665915+Piyush07r@users.noreply.github.com>
2025-12-04 15:51:18 +0530

    Create question.js

---
+++ data/question.js
@@ -0,0 +1,20 @@
+const questions = [
+    {
+        subject: "terraform",
+        difficulty: "easy",
+        question: "What is Terraform?",
+        Answer: "Terraform is an Infrastructure as Code (IaC) tool created by HashiCorp."
+    },
+    {
+        subject: "terraform",
+        difficulty: "Medium",
+        question: "What is a Terraform state file?",
+        Answer: "Terraform state is used to map real resources to your config."
+    },
+    {
+        subject: "terraform",
+        difficulty: "scenario",
+        question: "Your Terraform apply failed halfway—what will you do?",
+        Answer: "Check Terraform state, refresh it, plan again and fix corrupted resources."
+    }
+];
